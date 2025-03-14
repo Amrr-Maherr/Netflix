72754c0ce2901048a155277d4d93cac44eaffd5c
Amr maher <amrm80233@>
2025-03-14 16:28:09 +0200

    update style

--- src/app/AllMovies/page.jsx
+++ src/app/AllMovies/page.jsx
@@ -60,9 +60,9 @@ export default function AllMovies() {
 
   return (
     <>
-      <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32 min-h-screen">
+      <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto flex justify-center items-center min-h-screen">
+          <div className="mx-auto flex justify-center items-center">
             <div className="max-w-xl lg:max-w-lg text-center">
               {/* استخدام motion.img مع المتغيرات */}
               <motion.img
